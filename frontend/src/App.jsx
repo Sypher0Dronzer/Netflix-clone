b3f658ed142bcbafd4413086241c1d73b1382043
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-08-29 12:19:41 +0530

    b4 adding cast and crew details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import WatchPage from "./pages/WatchPage";
 
 export default function App() {
   const {user,isCheckingAuth,authCheck}= useAuthStore()
@@ -30,6 +31,7 @@ export default function App() {
           <Route index element={<HomePage />} />
           <Route path="/login" element={!user? <LoginPage /> : <Navigate to={'/'}/>} />
           <Route path="/signup" element={!user? <SignUpPage /> : <Navigate to={'/'}/>} />
+          <Route path="/watch/:id" element={user? <WatchPage /> : <Navigate to={'/login'}/>} />
       </Routes>
       <Toaster/>
       <Footer/>
