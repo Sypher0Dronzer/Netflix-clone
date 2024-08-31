e2d97b7444eeafc93849525f6fa118836d6f896a
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-08-31 15:40:12 +0530

    commit changes

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -7,7 +7,7 @@ export const useAuthStore = create((set) => ({
   isSigningUp: false,
   isCheckingAuth: true,
   isLoggingOut: true,
-  isLoggingIn: true,
+  isLoggingIn: false,
   signup: async (credentials) => {
     set({ isSigningUp: true });
     try {
@@ -22,7 +22,7 @@ export const useAuthStore = create((set) => ({
   },
 
   login: async (credentials) => {
-    set({ isLoggingIn: false });
+    set({ isLoggingIn: true });
     try {
       const response = await axios.post("/api/v1/auth/login", credentials);
 
