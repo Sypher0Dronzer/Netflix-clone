17d46d493d5788e1a42530532e6ab73d7975ea55
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-08-31 16:38:14 +0530

    basic changes

--- frontend/src/pages/components/Footer.jsx
+++ frontend/src/pages/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
 	return (
 		<footer className='py-4 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
-			<div className='flex flex-col items-center justify-between md:h-24 md:flex-row'>
-				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
+			<div className='md:py-8'>
+				<p className='text-center  text-sm '>
 					A creation of {" "}
 					<a
 						href='https://github.com/Sypher0Dronzer'
@@ -11,19 +11,10 @@ const Footer = () => {
 					>
 						Sypher Dronzer
 					</a>
+					{" "}😎
 					. 
 				</p>
-                <p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
-					Link to {" "}
-					<a
-						href='https://github.com/Sypher0Dronzer/Netflix-clone'
-						target='_blank'
-						className='font-medium hover:underline underline-offset-4 hover:text-red-500'
-					>
-						Repository
-					</a>
-					. 
-				</p>
+                
 			</div>
 		</footer>
 	);
